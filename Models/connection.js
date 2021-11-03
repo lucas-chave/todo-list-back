ce7a938fa2d20eb1345aee104a28fd4f9b3edb9a
lucas-chave <lcchavesr150@gmail.com>
2021-11-03 19:24:10 -0300

    feat: adding file connection in models layer

---
+++ Models/connection.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const DB_NAME = 'TodoList';
+
+let db = null;
+
+const connection = async () => {
+  if (db) return Promise.resolve(db);
+  const conn = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
+  db = conn.db(DB_NAME);
+  return db;
+};
+
+module.exports = {
+  connection
+};
